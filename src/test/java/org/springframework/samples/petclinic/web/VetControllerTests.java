141640a6c906fd88e6a6c92f055169f015e51e25
cargamram <cargamram@alum.us.es>
2020-03-30 21:12:55 +0200

    Test vetControllerTest

--- src/test/java/org/springframework/samples/petclinic/web/VetControllerTests.java
+++ src/test/java/org/springframework/samples/petclinic/web/VetControllerTests.java
@@ -1,78 +1,131 @@
+
 package org.springframework.samples.petclinic.web;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import java.util.Optional;
 
 import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
-import org.springframework.samples.petclinic.configuration.SecurityConfiguration;
-import org.springframework.samples.petclinic.model.Specialty;
-import org.springframework.samples.petclinic.model.Vet;
-import org.springframework.samples.petclinic.service.VetService;
-import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-
-import static org.hamcrest.xml.HasXPath.hasXPath;
-import static org.mockito.BDDMockito.given;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
+import org.springframework.samples.petclinic.configuration.SecurityConfiguration;
+import org.springframework.samples.petclinic.model.Authorities;
+import org.springframework.samples.petclinic.model.Clinic;
+import org.springframework.samples.petclinic.model.Manager;
+import org.springframework.samples.petclinic.model.Specialty;
+import org.springframework.samples.petclinic.model.User;
+import org.springframework.samples.petclinic.model.Vet;
+import org.springframework.samples.petclinic.service.ClinicService;
+import org.springframework.samples.petclinic.service.ManagerService;
+import org.springframework.samples.petclinic.service.VetService;
 import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.test.context.support.WithMockUser;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import org.springframework.test.web.servlet.MockMvc;
 
 /**
  * Test class for the {@link VetController}
  */
-@WebMvcTest(controllers=VetController.class,
-		excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = WebSecurityConfigurer.class),
-		excludeAutoConfiguration= SecurityConfiguration.class)
+@WebMvcTest(controllers = VetController.class,
+	excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = WebSecurityConfigurer.class),
+	excludeAutoConfiguration = SecurityConfiguration.class)
 class VetControllerTests {
 
-	@Autowired
-	private VetController vetController;
+	private static final int	TEST_VET_ID		= 1;
+
+	private static final int	TEST_MANAGER_ID	= 3;
 
 	@MockBean
-	private VetService clinicService;
+	private ClinicService		clinicService;
+
+	@MockBean
+	private VetService			vetService;
+
+	@MockBean
+	private ManagerService		managerService;
 
 	@Autowired
-	private MockMvc mockMvc;
+	private MockMvc				mockMvc;
+
 
 	@BeforeEach
 	void setup() {
 
 		Vet james = new Vet();
+		james.setId(TEST_VET_ID);
 		james.setFirstName("James");
 		james.setLastName("Carter");
-		james.setId(1);
+		james.setAddress("110 W. Liberty St.");
+		james.setCity("Madison");
+		james.setTelephone("6085551023");
+
+		Optional<Vet> opt = Optional.of(james);
+		BDDMockito.given(vetService.findById(TEST_VET_ID)).willReturn(opt);
+
 		Vet helen = new Vet();
 		helen.setFirstName("Helen");
 		helen.setLastName("Leary");
+		helen.setAddress("110 W. Liberty St.");
+		helen.setCity("Madison");
+		helen.setTelephone("6085551023");
 		helen.setId(2);
+
 		Specialty radiology = new Specialty();
 		radiology.setId(1);
 		radiology.setName("radiology");
-		helen.addSpecialty(radiology);
-		given(this.clinicService.findVets()).willReturn(Lists.newArrayList(james, helen));
+		james.addSpecialty(radiology);
+		BDDMockito.given(vetService.findVets()).willReturn(Lists.newArrayList(james, helen));
+
+		User user = new User();
+		user.setEnabled(true);
+		user.setUsername("pepito");
+		user.setPassword("pepito");
+
+		Authorities authority = new Authorities();
+		authority.setAuthority("manager");
+		authority.setUsername("pepito");
+		Manager pepe = new Manager();
+		pepe.setUser(user);
+		pepe.setId(TEST_MANAGER_ID);
+		pepe.setFirstName("Pepe");
+		pepe.setLastName("Leary");
+		pepe.setAddress("110 W. Liberty St.");
+		pepe.setCity("Madison");
+		pepe.setTelephone("6085551023");
+
+		Optional<Manager> manager = Optional.of(pepe);
+		BDDMockito.given(managerService.findManagerByUsername("pepito")).willReturn(manager);
+
+		Clinic clinic = new Clinic();
+		clinic.setId(1);
+		clinic.setCity("Sevilla");
+		BDDMockito.given(clinicService.findClinicByManagerId(TEST_MANAGER_ID)).willReturn(clinic);
+
+		//
 	}
-        
-    @WithMockUser(value = "spring")
-		@Test
-	void testShowVetListHtml() throws Exception {
-		mockMvc.perform(get("/vets")).andExpect(status().isOk()).andExpect(model().attributeExists("vets"))
-				.andExpect(view().name("vets/vetList"));
-	}	
 
 	@WithMockUser(value = "spring")
-        @Test
-	void testShowVetListXml() throws Exception {
-		mockMvc.perform(get("/vets.xml").accept(MediaType.APPLICATION_XML)).andExpect(status().isOk())
-				.andExpect(content().contentType(MediaType.APPLICATION_XML_VALUE))
-				.andExpect(content().node(hasXPath("/vets/vetList[id=1]/id")));
+	@Test
+	void testShowVetsAvailable() throws Exception {
+		mockMvc.perform(get("/vets/vetsAvailable")).andExpect(status().isOk())
+			.andExpect(model().attributeExists("vets2")).andExpect(view().name("vets/vetsAvailable"));
+	}
+
+	@WithMockUser(value = "pepito", authorities = {
+		"manager"
+	})
+	@Test
+	void testAcceptVet() throws Exception {
+		mockMvc.perform(get("/vets/accept/{vetId}", TEST_VET_ID)).andExpect(status().isFound())
+			.andExpect(view().name("redirect:/vets/vetsAvailable"));
 	}
 
 }
