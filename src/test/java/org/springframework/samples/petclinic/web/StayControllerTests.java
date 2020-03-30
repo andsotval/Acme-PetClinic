a15cf60642d0b00d47d3b2e6eac5cac79cfa65c4
Andres <andres_soto_7@hotmail.com>
2020-03-30 20:27:01 +0200

    Fix error

--- src/test/java/org/springframework/samples/petclinic/web/StayControllerTests.java
+++ src/test/java/org/springframework/samples/petclinic/web/StayControllerTests.java
@@ -1,13 +1,21 @@
 
 package org.springframework.samples.petclinic.web;
 
+import java.time.LocalDate;
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.samples.petclinic.configuration.SecurityConfiguration;
+import org.springframework.samples.petclinic.model.Stay;
 import org.springframework.samples.petclinic.service.StayService;
+import org.springframework.samples.petclinic.service.VetService;
 import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
@@ -23,43 +31,63 @@ class StayControllerTests {
 	@Autowired
 	private MockMvc				mockMvc;
 
-	@Autowired
+	@MockBean
 	private StayService			stayService;
 
+	@MockBean
+	private VetService			vetService;
+
+
+	@BeforeEach
+	void setup() {
+		LocalDate actualDate = LocalDate.now();
+
+		Stay stay = new Stay();
+		stay.setDescription("Description");
+		stay.setIsAccepted(null);
+		stay.setStartDate(actualDate);
+		stay.setFinishDate(actualDate);
+
+		Optional<Stay> optionalStay = Optional.of(stay);
+
+		BDDMockito.given(this.stayService.findById(StayControllerTests.TEST_STAY_ID)).willReturn(optionalStay);
+
+	}
 
 	//listAllPending (todas las stays devueltas tienen que tener isAcepted a null)
 	@WithMockUser(value = "spring")
 	@Test
 	void testShowStaysPending() throws Exception {
-		this.mockMvc.perform(MockMvcRequestBuilders.get("stays/listAllPending")).andExpect(MockMvcResultMatchers.status().isOk()).andExpect(MockMvcResultMatchers.model().attributeExists("stays")).andExpect(MockMvcResultMatchers.view().name("stays/list"));
+		this.mockMvc.perform(MockMvcRequestBuilders.get("/stays/listAllPending")).andExpect(MockMvcResultMatchers.status().isOk()).andExpect(MockMvcResultMatchers.model().attributeExists("stays")).andExpect(MockMvcResultMatchers.view().name("stays/list"));
 	}
 
 	//listAllAccepted (todas las stays devueltas tienen que tener isAcepted a true)
 	@WithMockUser(value = "spring")
 	@Test
 	void testShowStaysAccept() throws Exception {
-		this.mockMvc.perform(MockMvcRequestBuilders.get("stays/listAllAccepted")).andExpect(MockMvcResultMatchers.status().isOk()).andExpect(MockMvcResultMatchers.model().attributeExists("stays")).andExpect(MockMvcResultMatchers.view().name("stays/list"));
+		this.mockMvc.perform(MockMvcRequestBuilders.get("/stays/listAllAccepted")).andExpect(MockMvcResultMatchers.status().isOk()).andExpect(MockMvcResultMatchers.model().attributeExists("stays"))
+			.andExpect(MockMvcResultMatchers.view().name("stays/list"));
 	}
 
 	//acceptStay (pasarle una stay con isAccepted a null y te la actualice a true)
 	@WithMockUser(value = "spring")
 	@Test
 	void testAcceptStay() throws Exception {
-		this.mockMvc.perform(MockMvcRequestBuilders.get("stays/accept/{stayId}", StayControllerTests.TEST_STAY_ID)).andExpect(MockMvcResultMatchers.status().isOk()).andExpect(MockMvcResultMatchers.view().name("redirect:/stays/listAllPending"));
+		this.mockMvc.perform(MockMvcRequestBuilders.get("/stays/accept/{stayId}", StayControllerTests.TEST_STAY_ID)).andExpect(MockMvcResultMatchers.status().isFound()).andExpect(MockMvcResultMatchers.view().name("redirect:/stays/listAllAccepted"));
 	}
 
 	//cancelStay (pasarle una stay con isAccepted a null y te la actualice a false)
 	@WithMockUser(value = "spring")
 	@Test
 	void testCancelStay() throws Exception {
-		this.mockMvc.perform(MockMvcRequestBuilders.get("stays/cancel/{stayId}", StayControllerTests.TEST_STAY_ID)).andExpect(MockMvcResultMatchers.status().isOk()).andExpect(MockMvcResultMatchers.view().name("redirect:/stays/listAllPending"));
+		this.mockMvc.perform(MockMvcRequestBuilders.get("/stays/cancel/{stayId}", StayControllerTests.TEST_STAY_ID)).andExpect(MockMvcResultMatchers.status().isFound()).andExpect(MockMvcResultMatchers.view().name("redirect:/stays/listAllAccepted"));
 	}
 
 	//changeDateStay (la stay que entra es la misma que sale)
 	@WithMockUser(value = "spring")
 	@Test
 	void testChangeDateStay() throws Exception {
-		this.mockMvc.perform(MockMvcRequestBuilders.get("stays/changeDate/{stayId}", StayControllerTests.TEST_STAY_ID)).andExpect(MockMvcResultMatchers.status().isOk()).andExpect(MockMvcResultMatchers.view().name("stays/createOrUpdateStayForm"));
+		this.mockMvc.perform(MockMvcRequestBuilders.get("/stays/changeDate/{stayId}", StayControllerTests.TEST_STAY_ID)).andExpect(MockMvcResultMatchers.status().isOk()).andExpect(MockMvcResultMatchers.view().name("/stays/createOrUpdateStayForm"));
 	}
 
 	//updateStay (actualizar parametros (startDate, finishDate y description) y comprobar que se ha guardado bien)
@@ -69,8 +97,9 @@ class StayControllerTests {
 	@Test
 	void testStaySuccesfull() throws Exception {
 		this.mockMvc
-			.perform(MockMvcRequestBuilders.post("stays/save/{stayId}", StayControllerTests.TEST_STAY_ID).with(SecurityMockMvcRequestPostProcessors.csrf()).param("description", "Description").param("startDate", "2020-06-09").param("endDate", "2020-07-09"))
-			.andExpect(MockMvcResultMatchers.model().attributeHasErrors("stay")).andExpect(MockMvcResultMatchers.status().isOk()).andExpect(MockMvcResultMatchers.view().name("pets/createOrUpdateVisitForm"));
+			.perform(
+				MockMvcRequestBuilders.post("/stays/save/{stayId}", StayControllerTests.TEST_STAY_ID).with(SecurityMockMvcRequestPostProcessors.csrf()).param("description", "Description").param("startDate", "2020-06-09").param("endDate", "2020-07-09"))
+			.andExpect(MockMvcResultMatchers.status().isOk()).andExpect(MockMvcResultMatchers.view().name("/stays/createOrUpdateStayForm"));
 	}
 
 }
