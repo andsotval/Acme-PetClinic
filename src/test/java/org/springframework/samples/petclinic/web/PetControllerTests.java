cfd394293cec8b7e012c1beda4f684cb76e806df
mpg97 <migpatgut@alum.us.es>
2020-04-28 02:42:54 +0200

    pruebas creadas y limpieza de codigo en controlador

--- src/test/java/org/springframework/samples/petclinic/web/PetControllerTests.java
+++ src/test/java/org/springframework/samples/petclinic/web/PetControllerTests.java
@@ -1,23 +1,42 @@
 
 package org.springframework.samples.petclinic.web;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.mockito.BDDMockito;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.samples.petclinic.configuration.SecurityConfiguration;
+import org.springframework.samples.petclinic.model.Authorities;
+import org.springframework.samples.petclinic.model.Clinic;
 import org.springframework.samples.petclinic.model.Owner;
 import org.springframework.samples.petclinic.model.Pet;
 import org.springframework.samples.petclinic.model.PetType;
+import org.springframework.samples.petclinic.model.User;
 import org.springframework.samples.petclinic.service.OwnerService;
 import org.springframework.samples.petclinic.service.PetService;
 import org.springframework.samples.petclinic.service.PetTypeService;
+import org.springframework.samples.petclinic.service.StayService;
+import org.springframework.samples.petclinic.service.VisitService;
 import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
 
 /**
  * Test class for the {@link PetController}
@@ -28,11 +47,18 @@ import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 	includeFilters = @ComponentScan.Filter(value = PetTypeFormatter.class, type = FilterType.ASSIGNABLE_TYPE),
 	excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = WebSecurityConfigurer.class),
 	excludeAutoConfiguration = SecurityConfiguration.class)
-class PetControllerTests {
+public class PetControllerTests {
 
 	private static final int	TEST_OWNER_ID	= 1;
 
 	private static final int	TEST_PET_ID		= 1;
+	private static final int	TEST_PET_ID_2	= 2;
+
+	private static Pet			pet1;
+	private static Pet			pet2;
+
+	@Autowired
+	private MockMvc				mockMvc;
 
 	@MockBean
 	private PetService			petService;
@@ -43,26 +69,155 @@ class PetControllerTests {
 	@MockBean
 	private OwnerService		ownerService;
 
+	@MockBean
+	private VisitService		visitService;
+
+	@MockBean
+	private StayService			stayService;
+
 
 	@BeforeEach
 	void setup() {
+
+		Clinic clinic = new Clinic();
+		clinic.setId(1);
+		clinic.setName("Clinic 1");
+		clinic.setCity("City");
+		clinic.setAddress("Address");
+		clinic.setTelephone("642968344");
+
+		User userOwner = new User();
+		userOwner.setEnabled(true);
+		userOwner.setId(TEST_OWNER_ID);
+		userOwner.setUsername("George");
+		userOwner.setPassword("George");
+
+		Authorities authorityOwner = new Authorities();
+		authorityOwner.setAuthority("owner");
+		authorityOwner.setUsername("George");
+
+		Owner owner = new Owner();
+		owner.setId(TEST_OWNER_ID);
+		owner.setUser(userOwner);
+		owner.setFirstName("George");
+		owner.setLastName("Franklin");
+		owner.setAddress("110 W. Liberty St.");
+		owner.setCity("Madison");
+		owner.setMail("owner@owner.com");
+		owner.setTelephone("6085551023");
+		owner.setClinic(clinic);
+
+		Optional<Owner> optOwner = Optional.of(owner);
+		BDDMockito.given(ownerService.findEntityById(TEST_OWNER_ID)).willReturn(optOwner);
+		BDDMockito.given(ownerService.findPersonByUsername("George")).willReturn(owner);
+
 		PetType cat = new PetType();
-		cat.setId(3);
-		cat.setName("hamster");
+		cat.setId(1);
+		cat.setName("cat");
+		cat.setAvailable(true);
 
-		Owner george = new Owner();
-		george.setId(TEST_OWNER_ID);
-		george.setFirstName("George");
-		george.setLastName("Franklin");
-		george.setAddress("110 W. Liberty St.");
-		george.setCity("Madison");
-		george.setTelephone("6085551023");
+		PetType dog = new PetType();
+		dog.setId(2);
+		dog.setName("dog");
+		dog.setAvailable(true);
 
-		Optional<Owner> owner = Optional.of(george);
+		PetType rabbit = new PetType();
+		rabbit.setId(3);
+		rabbit.setName("rabbit");
+		rabbit.setAvailable(true);
 
-		BDDMockito.given(petTypeService.findAvailable()).willReturn(Lists.newArrayList(cat));
-		BDDMockito.given(ownerService.findEntityById(TEST_OWNER_ID)).willReturn(owner);
-		BDDMockito.given(petService.findEntityById(TEST_PET_ID).get()).willReturn(new Pet());
+		List<PetType> petTypeList = new ArrayList<PetType>();
+		petTypeList.add(cat);
+		petTypeList.add(dog);
+		petTypeList.add(rabbit);
+
+		BDDMockito.given(petTypeService.findAvailable()).willReturn(petTypeList);
+
+		Pet pet1 = new Pet();
+		pet1.setId(TEST_PET_ID);
+		pet1.setName("toby");
+		pet1.setOwner(owner);
+		pet1.setType(dog);
+		LocalDate birthday = LocalDate.of(2019, 7, 15);
+		pet1.setBirthDate(birthday);
+		PetControllerTests.pet1 = pet1;
+
+		Optional<Pet> toby = Optional.of(pet1);
+		BDDMockito.given(petService.findEntityById(TEST_PET_ID)).willReturn(toby);
+
+		Pet pet2 = new Pet();
+		pet2.setId(TEST_PET_ID_2);
+		pet2.setName("coco");
+		pet2.setOwner(owner);
+		pet2.setType(cat);
+		LocalDate birthday2 = LocalDate.of(2019, 11, 29);
+		pet2.setBirthDate(birthday2);
+		PetControllerTests.pet2 = pet2;
+
+		Optional<Pet> coco = Optional.of(pet2);
+		BDDMockito.given(petService.findEntityById(TEST_PET_ID_2)).willReturn(coco);
+
+		List<Pet> ownerPets = Lists.newArrayList(pet1, pet2);
+		BDDMockito.given(petService.findPetsByOwnerId(TEST_OWNER_ID)).willReturn(ownerPets);
+	}
+
+	@WithMockUser(value = "George")
+	@Test
+	void testListMyPets() throws Exception {
+		mockMvc.perform(get("/pets/listMyPets")).andExpect(status().isOk()).andExpect(model().attributeExists("types"))
+			.andExpect(model().attributeExists("pets")).andExpect(model().attributeExists("ownerId"))
+			.andExpect(view().name("pets/list"));
+	}
+
+	@WithMockUser(value = "George")
+	@Test
+	void testNewPet() throws Exception {
+		mockMvc.perform(get("/pets/new/{ownerId}", TEST_OWNER_ID)).andExpect(status().isOk())
+			.andExpect(model().attributeExists("pet")).andExpect(model().attributeExists("types"))
+			.andExpect(view().name("pets/createOrUpdatePetForm"));
+	}
+
+	@WithMockUser(value = "George")
+	@Test
+	void testSavePet() throws Exception {
+		mockMvc
+			.perform(post("/pets/save").with(csrf()).param("name", "rex").param("birthDate", "2019/04/11")
+				.param("type", "dog").param("owner.id", String.valueOf(TEST_OWNER_ID)))
+			.andExpect(status().is3xxRedirection())
+			/* .andExpect(model().attribute("message", "Stay succesfully updated")) */
+			.andExpect(view().name("redirect:/pets/listMyPets"));
+
+	}
+
+	@WithMockUser(value = "George")
+	@Test
+	void testCancelVisit() throws Exception {
+		mockMvc.perform(get("/pets/delete/{petId}", TEST_PET_ID)).andExpect(status().is3xxRedirection())
+			.andExpect(view().name("redirect:/pets/listMyPets"));
+	}
+
+	@WithMockUser(value = "George")
+	@Test
+	void testNewVisit() throws Exception {
+		mockMvc.perform(get("/pets/newVisit/{petId}", TEST_PET_ID_2)).andExpect(status().isOk())
+			.andExpect(model().attributeExists("clinicId")).andExpect(model().attributeExists("visit"))
+			.andExpect(model().attributeExists("visits")).andExpect(view().name("visits/createOrUpdateVisitForm"));
+	}
+
+	@WithMockUser(value = "George")
+	@Test
+	void testNewStay() throws Exception {
+		mockMvc.perform(get("/pets/newStay/{petId}", TEST_PET_ID)).andExpect(status().isOk())
+			.andExpect(model().attributeExists("clinicId")).andExpect(model().attributeExists("stay"))
+			.andExpect(model().attributeExists("stays")).andExpect(view().name("stays/createOrUpdateStayForm"));
+	}
+
+	@WithMockUser(value = "George")
+	@Test
+	void testInitUpdateForm() throws Exception {
+		mockMvc.perform(get("/pets/{petId}/edit", TEST_PET_ID_2)).andExpect(status().isOk())
+			.andExpect(model().attributeExists("pet")).andExpect(model().attribute("pet", pet2))
+			.andExpect(view().name("pets/createOrUpdatePetForm"));
 	}
 
 	//	@WithMockUser(value = "spring")
