dcb567669f99dc535dc47f3b26a4f26fefa8fa43
Andres <andres.soto@ecommfarm.com>
2020-04-28 17:17:22 +0200

    Test

--- src/test/java/org/springframework/samples/petclinic/web/ClinicControllerTests.java
+++ src/test/java/org/springframework/samples/petclinic/web/ClinicControllerTests.java
@@ -2,6 +2,8 @@
 package org.springframework.samples.petclinic.web;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -15,8 +17,11 @@ import org.springframework.context.annotation.FilterType;
 import org.springframework.samples.petclinic.configuration.SecurityConfiguration;
 import org.springframework.samples.petclinic.model.Authorities;
 import org.springframework.samples.petclinic.model.Clinic;
+import org.springframework.samples.petclinic.model.Owner;
+import org.springframework.samples.petclinic.model.Stay;
 import org.springframework.samples.petclinic.model.User;
 import org.springframework.samples.petclinic.model.Vet;
+import org.springframework.samples.petclinic.model.Visit;
 import org.springframework.samples.petclinic.service.ClinicService;
 import org.springframework.samples.petclinic.service.OwnerService;
 import org.springframework.samples.petclinic.service.StayService;
@@ -28,12 +33,14 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-@WebMvcTest(controllers = ClinicController.class,
-	excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = WebSecurityConfigurer.class),
-	excludeAutoConfiguration = SecurityConfiguration.class)
-class ClinicControllerTests {
+@WebMvcTest(controllers = ClinicController.class, excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = WebSecurityConfigurer.class), excludeAutoConfiguration = SecurityConfiguration.class)
+public class ClinicControllerTests {
 
 	private static final int	TEST_VET_ID		= 1;
+	
+	private static final int	TEST_OWNER_ID_1	= 1;
+	
+	private static final int	TEST_OWNER_ID_2	= 2;
 
 	private static final int	TEST_CLINIC_ID	= 1;
 
@@ -48,10 +55,10 @@ class ClinicControllerTests {
 
 	@MockBean
 	private OwnerService		ownerService;
-	
+
 	@MockBean
 	private StayService			stayService;
-	
+
 	@MockBean
 	private VisitService		visitService;
 
@@ -81,21 +88,97 @@ class ClinicControllerTests {
 		pepe.setTelephone("6085551023");
 		pepe.setClinic(clinic);
 
-		BDDMockito.given(vetService.findPersonByUsername("pepito")).willReturn(pepe);
+		User userOwner = new User();
+		userOwner.setEnabled(true);
+		userOwner.setUsername("joselito");
+		userOwner.setPassword("joselito");
+		
+		Authorities authorityOwner = new Authorities();
+		authorityOwner.setAuthority("Owner");
+		authorityOwner.setUsername("joselito");
+		Owner joselito = new Owner();
+		joselito.setUser(userOwner);
+		joselito.setId(TEST_OWNER_ID_1);
+		joselito.setFirstName("Joselito");
+		joselito.setLastName("Leary");
+		joselito.setAddress("110 W. Liberty St.");
+		joselito.setCity("Madison");
+		joselito.setTelephone("6085551023");
+		joselito.setClinic(clinic);
+		
+		User userOwner2 = new User();
+		userOwner2.setEnabled(true);
+		userOwner2.setUsername("manolito");
+		userOwner2.setPassword("manolito");
+		
+		Authorities authorityOwner2 = new Authorities();
+		authorityOwner2.setAuthority("Owner");
+		authorityOwner2.setUsername("manolito");
+		Owner manolito = new Owner();
+		manolito.setUser(userOwner2);
+		manolito.setId(TEST_OWNER_ID_2);
+		manolito.setFirstName("Manolito");
+		manolito.setLastName("Leary");
+		manolito.setAddress("110 W. Liberty St.");
+		manolito.setCity("Madison");
+		manolito.setTelephone("6085551023");
+		manolito.setClinic(null);
+		
+		List<Visit> visitVacia = new ArrayList<Visit>();
+		List<Stay> stayVacia = new ArrayList<Stay>();
 
+		
+		BDDMockito.given(vetService.findPersonByUsername("pepito")).willReturn(pepe);
+		
+		BDDMockito.given(ownerService.findPersonByUsername("joselito")).willReturn(joselito);
+		
+		BDDMockito.given(ownerService.findPersonByUsername("manolito")).willReturn(manolito);
+		
+		BDDMockito.given(visitService.findAllAcceptedByOwner(joselito)).willReturn(visitVacia);
+		
+		BDDMockito.given(visitService.findAllPendingByOwner(joselito)).willReturn(visitVacia);
+
+		BDDMockito.given(stayService.findAllAcceptedByOwner(joselito)).willReturn(stayVacia);
+		
+		BDDMockito.given(stayService.findAllPendingByOwner(joselito)).willReturn(stayVacia);
+		
 		Optional<Vet> optVet = Optional.of(pepe);
 		BDDMockito.given(vetService.findEntityById(TEST_VET_ID)).willReturn(optVet);
+		Optional<Owner> optOwner = Optional.of(joselito);
+		BDDMockito.given(ownerService.findEntityById(TEST_OWNER_ID_1)).willReturn(optOwner);
+		Optional<Owner> optOwner2 = Optional.of(manolito);
+		BDDMockito.given(ownerService.findEntityById(TEST_OWNER_ID_2)).willReturn(optOwner2);
 
 	}
 
 	//Show Clinic from a Vet
 	@WithMockUser(value = "pepito")
 	@Test
-	void testShowClinic() throws Exception {
+	void testShowClinicVet() throws Exception {
 		mockMvc.perform(MockMvcRequestBuilders.get("/clinics/getDetail"))
 			.andExpect(MockMvcResultMatchers.status().isOk())
-			.andExpect(MockMvcResultMatchers.model().attributeExists("clinic"))
-			.andExpect(MockMvcResultMatchers.view().name("clinics/show"));
+			//.andExpect(MockMvcResultMatchers.model().attributeExists("clinic"))
+			.andExpect(MockMvcResultMatchers.view().name("/clinics/clinicDetails"));
+	}
+	
+	//Show Clinic from a Owner
+	@WithMockUser(value = "joselito")
+	@Test
+	void testShowClinicOwner() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.get("/clinics/owner"))
+			.andExpect(MockMvcResultMatchers.status().is3xxRedirection())
+			//.andExpect(MockMvcResultMatchers.model().attributeExists("clinic"))
+			.andExpect(MockMvcResultMatchers.view().name("redirect:/clinics/getDetail"));
+	}
+	
+	//Show Clinic from a Owner
+	@WithMockUser(value = "manolito")
+	@Test
+	void testShowListClinicOwner() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.get("/clinics/owner"))
+			.andExpect(MockMvcResultMatchers.status().isOk())
+			//.andExpect(MockMvcResultMatchers.model().attributeExists("clinic"))
+			.andExpect(MockMvcResultMatchers.view().name("/clinics/owner/clinicsList"));
 	}
 
 }
