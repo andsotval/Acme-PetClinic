3aa61e9b47e0c4da9db5e766505c191563a41b0b
LuisGalanRuiz <luigalrui@alum.us.es>
2020-04-30 01:29:45 +0200

    UI Finished

--- src/test/java/org/springframework/samples/petclinic/ui/NewStayAsOwnerUITest.java
+++ src/test/java/org/springframework/samples/petclinic/ui/NewStayAsOwnerUITest.java
@@ -1,16 +1,13 @@
 package org.springframework.samples.petclinic.ui;
 
-import java.util.regex.Pattern;
 import java.time.LocalDate;
 import java.util.concurrent.TimeUnit;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 import static org.junit.Assert.*;
-import static org.hamcrest.CoreMatchers.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.Select;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -27,28 +24,20 @@ public class NewStayAsOwnerUITest {
 	private int port;
 	
 	private WebDriver driver;
-	private String baseUrl;
-	private boolean acceptNextAlert = true;
 	private StringBuffer verificationErrors = new StringBuffer();
 
 	@BeforeEach
 	public void setUp() throws Exception {
 		driver = new FirefoxDriver();
-		baseUrl = "https://www.google.com/";
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 	}
 
 	@Test
 	public void testUntitledTestCase() throws Exception {
-		driver.get("http://localhost:" + port /*+ "/login"*/);
-	    driver.findElement(By.xpath("//a[contains(text(),'Login')]")).click();
-	    driver.findElement(By.id("username")).click();
-	    driver.findElement(By.id("username")).clear();
-	    driver.findElement(By.id("username")).sendKeys("owner1");
-	    driver.findElement(By.id("password")).click();
-	    driver.findElement(By.id("password")).clear();
-	    driver.findElement(By.id("password")).sendKeys("owner1");
-	    driver.findElement(By.xpath("//button[@type='submit']")).click();
+		driver.get("http://localhost:" + port);
+	    
+		LogInAsOwner();
+
 	    driver.findElement(By.xpath("//div[@id='main-navbar']/ul/li[2]/a/span[2]")).click();
 	    driver.findElement(By.xpath("//table[@id='petsTable']/tbody/tr/td")).click();
 	    assertEquals("Leo", driver.findElement(By.linkText("Leo")).getText());
@@ -85,9 +74,8 @@ public class NewStayAsOwnerUITest {
 	    assertEquals("My Stays", driver.findElement(By.xpath("//h1")).getText());
 	    driver.findElement(By.xpath("//h2[2]")).click();
 	    assertEquals("Stays waiting for Acceptance", driver.findElement(By.xpath("//h2[2]")).getText());
-	    driver.findElement(By.xpath("//div[@id='main-navbar']/ul[2]/li/a/strong")).click();
-	    driver.findElement(By.linkText("Logout")).click();
-	    driver.findElement(By.xpath("//button[@type='submit']")).click();
+	    
+	    LogOut();
 	}
 
 	private String dateToString(LocalDate date) {
@@ -115,6 +103,25 @@ public class NewStayAsOwnerUITest {
 		}
 		return date.getYear() + "-" + s + date.getMonthValue() + "-" + r + date.getDayOfMonth();
 	}
+	
+	private WebDriver LogInAsOwner() {
+	    driver.findElement(By.xpath("//a[contains(text(),'Login')]")).click();
+	    driver.findElement(By.id("username")).click();
+	    driver.findElement(By.id("username")).clear();
+	    driver.findElement(By.id("username")).sendKeys("owner1");
+	    driver.findElement(By.id("password")).click();
+	    driver.findElement(By.id("password")).clear();
+	    driver.findElement(By.id("password")).sendKeys("owner1");
+	    driver.findElement(By.xpath("//button[@type='submit']")).click();
+		return driver;	
+	}
+	
+	private WebDriver LogOut() {
+	    driver.findElement(By.xpath("//div[@id='main-navbar']/ul[2]/li/a/strong")).click();
+	    driver.findElement(By.linkText("Logout")).click();
+	    driver.findElement(By.xpath("//button[@type='submit']")).click();
+		return driver;
+	}
 
 	@AfterEach
 	public void tearDown() throws Exception {
@@ -125,36 +132,4 @@ public class NewStayAsOwnerUITest {
 		}
 	}
 
-	private boolean isElementPresent(By by) {
-		try {
-			driver.findElement(by);
-			return true;
-		} catch (NoSuchElementException e) {
-			return false;
-		}
-	}
-
-	private boolean isAlertPresent() {
-		try {
-			driver.switchTo().alert();
-			return true;
-		} catch (NoAlertPresentException e) {
-			return false;
-		}
-	}
-
-	private String closeAlertAndGetItsText() {
-		try {
-			Alert alert = driver.switchTo().alert();
-			String alertText = alert.getText();
-			if (acceptNextAlert) {
-				alert.accept();
-			} else {
-				alert.dismiss();
-			}
-			return alertText;
-		} finally {
-			acceptNextAlert = true;
-		}
-	}
 }
