b0cf93daf694d08ba4d4bb6170df291bca15cb96
cargamram <cargamram@alum.us.es>
2020-04-29 18:43:51 +0200

    Añadida prueba negativa en PetTypeServiceTest

--- src/test/java/org/springframework/samples/petclinic/service/PetTypeServiceTest.java
+++ src/test/java/org/springframework/samples/petclinic/service/PetTypeServiceTest.java
@@ -5,14 +5,21 @@ import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.Collection;
+import java.util.Locale;
 import java.util.Optional;
+import java.util.Set;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validator;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.samples.petclinic.model.PetType;
 import org.springframework.stereotype.Service;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 @DataJpaTest(includeFilters = @ComponentScan.Filter(Service.class))
 public class PetTypeServiceTest {
@@ -21,16 +28,26 @@ public class PetTypeServiceTest {
 	protected PetTypeService service;
 
 
+	private Validator createValidator() {
+		LocalValidatorFactoryBean localValidatorFactoryBean = new LocalValidatorFactoryBean();
+		localValidatorFactoryBean.afterPropertiesSet();
+		return localValidatorFactoryBean;
+	}
+
 	@Test
 	public void testFindAvailable() {
 		Collection<PetType> collection = service.findAvailable();
 		assertEquals(collection.size(), 6);
+
+		collection.forEach(pettype -> assertEquals(pettype.getAvailable(), true));
 	}
 
 	@Test
 	public void testFindNotAvailable() {
 		Collection<PetType> collection = service.findNotAvailable();
 		assertEquals(collection.size(), 1);
+
+		collection.forEach(pettype -> assertEquals(pettype.getAvailable(), false));
 	}
 
 	@Test
@@ -46,7 +63,7 @@ public class PetTypeServiceTest {
 	}
 
 	@Test
-	public void testSaveEntity() {
+	public void testSaveEntityPositive() {
 		Collection<PetType> collection = (Collection<PetType>) service.findAllEntities();
 		assertEquals(collection.size(), 7);
 
@@ -62,6 +79,22 @@ public class PetTypeServiceTest {
 		assertTrue(newEntity.isPresent());
 		assertEquals(newEntity.get().getAvailable(), true);
 		assertEquals(newEntity.get().getName(), "name1");
+	}
+
+	@Test
+	public void testSaveEntityNegative() {
+		LocaleContextHolder.setLocale(Locale.ENGLISH);
+		PetType entity = new PetType();
+		entity.setName("name1");
+
+		Validator validator = createValidator();
+		Set<ConstraintViolation<PetType>> constraintViolations = validator.validate(entity);
+		assertEquals(constraintViolations.size(), 1);
+
+		ConstraintViolation<PetType> violation = constraintViolations.iterator().next();
+		assertEquals(violation.getPropertyPath().toString(), "available");
+		assertEquals(violation.getMessage(), "must not be null");
+
 	}
 
 	@Test
