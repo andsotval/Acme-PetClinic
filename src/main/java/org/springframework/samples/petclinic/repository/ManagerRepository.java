119c8e935dee52361e7d9f6fa50298f057092505
mpg97 <migpatgut@alum.us.es>
2020-05-01 04:37:34 +0200

    correciones menores

--- src/main/java/org/springframework/samples/petclinic/repository/ManagerRepository.java
+++ src/main/java/org/springframework/samples/petclinic/repository/ManagerRepository.java
@@ -1,14 +1,10 @@
 
 package org.springframework.samples.petclinic.repository;
 
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.samples.petclinic.model.Manager;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ManagerRepository extends BaseRepository<Manager> {
-
-	@Query("SELECT manager FROM Manager manager WHERE manager.user.username=?1")
-	Manager findManagerByUsername(String user);
 
 }
