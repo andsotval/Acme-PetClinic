e45b7c7ed48a3804ba3b9fa1f2f17d556982bf2c
Andres <andres.soto@ecommfarm.com>
2020-05-02 00:49:16 +0200

    Test fixes

--- src/test/java/org/springframework/samples/petclinic/service/ClinicServiceTests.java
+++ src/test/java/org/springframework/samples/petclinic/service/ClinicServiceTests.java
@@ -3,23 +3,41 @@ package org.springframework.samples.petclinic.service;
 
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Locale;
+import java.util.Optional;
+import java.util.Set;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validator;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.samples.petclinic.model.Clinic;
 import org.springframework.samples.petclinic.model.Pet;
 import org.springframework.stereotype.Service;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 @DataJpaTest(includeFilters = @ComponentScan.Filter(Service.class))
-public class ClinicServiceTest {
+public class ClinicServiceTests {
 
 	@Autowired
 	protected ClinicService clinicService;
 
+
+	private Validator createValidator() {
+		LocalValidatorFactoryBean localValidatorFactoryBean = new LocalValidatorFactoryBean();
+		localValidatorFactoryBean.afterPropertiesSet();
+		return localValidatorFactoryBean;
+	}
 
 	@Test
 	public void TestFindClinicByManagerIdPositive() {
@@ -79,5 +97,100 @@ public class ClinicServiceTest {
 		assertNull(clinic);
 		Iterable<Pet> pets = clinicService.findPetsCyClinic(clinic);
 		assertNotNull(pets);
+	}
+
+	@Test
+	public void testFindAllEntities() {
+		Collection<Clinic> collection = (Collection<Clinic>) clinicService.findAllEntities();
+		assertEquals(collection.size(), 8);
+	}
+
+	@Test
+	public void testFindEntityByIdPositive() {
+		Optional<Clinic> entity = clinicService.findEntityById(1);
+		assertTrue(entity.isPresent());
+		assertTrue(entity.get().getId().equals(1));
+	}
+
+	@Test
+	public void testFindEntityByIdNegative() {
+		Optional<Clinic> entity = clinicService.findEntityById(99);
+		assertTrue(!entity.isPresent());
+	}
+
+	@Test
+	public void testSaveEntityPositive() {
+		Collection<Clinic> collection = (Collection<Clinic>) clinicService.findAllEntities();
+		assertEquals(collection.size(), 8);
+
+		Clinic entity = new Clinic();
+		entity.setName("Name 1");
+		entity.setAddress("Address 1");
+		entity.setCity("City 1");
+		entity.setTelephone("912654555");
+
+		collection = (Collection<Clinic>) clinicService.findAllEntities();
+		assertEquals(collection.size(), 4);
+
+		Optional<Clinic> newEntity = clinicService.findEntityById(5);
+		assertTrue(newEntity.isPresent());
+		assertEquals(newEntity.get().getName(), "Name 1");
+		assertEquals(newEntity.get().getAddress(), "Address 1");
+		assertEquals(newEntity.get().getCity(), "City 1");
+		assertEquals(newEntity.get().getTelephone(), "912654555");
+	}
+
+	@Test
+	public void testSaveEntityNegative() {
+		LocaleContextHolder.setLocale(Locale.ENGLISH);
+		Clinic entity = new Clinic();
+		entity.setName("Name 1");
+		entity.setAddress(null);
+		entity.setCity(null);
+		entity.setTelephone(null);
+
+		Validator validator = createValidator();
+		Set<ConstraintViolation<Clinic>> constraintViolations = validator.validate(entity);
+		assertEquals(constraintViolations.size(), 3);
+		Iterator<ConstraintViolation<Clinic>> it = constraintViolations.iterator();
+		while (it.hasNext()) {
+			ConstraintViolation<Clinic> violation = it.next();
+			String message = violation.getMessage();
+
+			switch (violation.getPropertyPath().toString()) {
+			case "description":
+				assertTrue(message.equals("must not be empty"));
+				break;
+			case "address":
+				assertTrue(message.equals("must not be empty"));
+				break;
+			case "city":
+				assertTrue(message.equals("must not be empty"));
+				break;
+			case "telephone":
+				assertTrue(message.equals("must not be empty"));
+				break;
+			default:
+				break;
+			}
+		}
+	}
+
+	@Test
+	public void testDeleteEntity() {
+		Optional<Clinic> entity = clinicService.findEntityById(1);
+		assertTrue(entity.isPresent());
+		clinicService.deleteEntity(entity.get());
+
+		Optional<Clinic> deleteEntity = clinicService.findEntityById(1);
+		assertTrue(!deleteEntity.isPresent());
+	}
+
+	@Test
+	public void testDeleteEntityById() {
+		clinicService.deleteEntityById(1);
+
+		Optional<Clinic> entity = clinicService.findEntityById(1);
+		assertTrue(!entity.isPresent());
 	}
 }
