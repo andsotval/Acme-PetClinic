b30f53d25972c91c4d8ea5c28e113c5e09701f1f
cargamram <cargamram@alum.us.es>
2020-05-17 19:51:45 +0200

    Añadidas pruebas unitarias de servicios y controladores para SpecialtyVetController y SpecialtyAdminController

--- src/test/java/org/springframework/samples/petclinic/service/ProductServiceTests.java
+++ src/test/java/org/springframework/samples/petclinic/service/ProductServiceTests.java
@@ -1,3 +1,8 @@
+/**
+ * DP2 - Grupo 8
+ * LAB F1.33
+ * Date: 17-may-2020
+ */
 
 package org.springframework.samples.petclinic.service;
 
@@ -26,7 +31,7 @@ import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 @ActiveProfiles("hsqldb")
 @DataJpaTest(includeFilters = @ComponentScan.Filter(Service.class))
-public class ProductServiceTest {
+public class ProductServiceTests {
 
 	private int					TEST_PRODUCT_ID					= 1;
 
