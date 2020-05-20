b64043de7d5631d7c92e51cff6fb43c55cd31489
cargamram <cargamram@alum.us.es>
2020-05-20 19:36:20 +0200

    Añadidas pruebas de integración y end2end para SpecialtyVetController y SpecialtyAdminController

--- src/test/java/org/springframework/samples/petclinic/e2e/SpecialtyAdminControllerE2ETests.java
+++ src/test/java/org/springframework/samples/petclinic/e2e/SpecialtyAdminControllerE2ETests.java
@@ -1,42 +1,29 @@
-/**
- * DP2 - Grupo 8
- * LAB F1.33
- * Date: 17-may-2020
- */
 
-package org.springframework.samples.petclinic.web;
+package org.springframework.samples.petclinic.e2e;
 
-import java.util.Optional;
-
-import org.assertj.core.util.Lists;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.BDDMockito;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.FilterType;
-import org.springframework.samples.petclinic.configuration.SecurityConfiguration;
-import org.springframework.samples.petclinic.model.Specialty;
-import org.springframework.samples.petclinic.service.SpecialtyService;
-import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.transaction.annotation.Transactional;
 
-@ActiveProfiles("hsqldb")
-@WebMvcTest(value = SpecialtyAdminController.class,
-	excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = WebSecurityConfigurer.class),
-	excludeAutoConfiguration = SecurityConfiguration.class)
-public class SpecialtyAdminControllerTests {
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@AutoConfigureMockMvc
+@Transactional
+@DirtiesContext(classMode = ClassMode.BEFORE_CLASS)
+public class SpecialtyAdminControllerE2ETests {
 
 	private static final int	TEST_SPECIALTY_ID_1					= 1;
-
-	private static final int	TEST_SPECIALTY_ID_2					= 2;
 
 	private static final int	TEST_SPECIALTY_ID_99				= 99;
 
@@ -49,29 +36,10 @@ public class SpecialtyAdminControllerTests {
 	@Autowired
 	private MockMvc				mockMvc;
 
-	@MockBean
-	private SpecialtyService	specialtyService;
 
-
-	@BeforeEach
-	void setup() {
-		Specialty specialty1 = new Specialty();
-		specialty1.setId(TEST_SPECIALTY_ID_1);
-		specialty1.setName("type1");
-		specialty1.setAvailable(true);
-
-		Specialty specialty2 = new Specialty();
-		specialty2.setId(TEST_SPECIALTY_ID_2);
-		specialty2.setName("type2");
-		specialty2.setAvailable(true);
-
-		BDDMockito.given(specialtyService.findAvailable()).willReturn(Lists.newArrayList(specialty1, specialty2));
-
-		Optional<Specialty> optSpecialty = Optional.of(specialty1);
-		BDDMockito.given(specialtyService.findEntityById(TEST_SPECIALTY_ID_1)).willReturn(optSpecialty);
-	}
-
-	@WithMockUser(value = "spring")
+	@WithMockUser(value = "admin", authorities = {
+		"admin"
+	})
 	@Test
 	void testListAvailable() throws Exception {
 		mockMvc.perform(MockMvcRequestBuilders.get("/specialty/admin/listAvailable"))
@@ -81,7 +49,9 @@ public class SpecialtyAdminControllerTests {
 			.andExpect(MockMvcResultMatchers.view().name(VIEW_SPECIALTY_ADMIN_LIST));
 	}
 
-	@WithMockUser(value = "spring")
+	@WithMockUser(value = "admin", authorities = {
+		"admin"
+	})
 	@Test
 	void testListNotAvailable() throws Exception {
 		mockMvc.perform(MockMvcRequestBuilders.get("/specialty/admin/listNotAvailable"))
@@ -91,7 +61,9 @@ public class SpecialtyAdminControllerTests {
 			.andExpect(MockMvcResultMatchers.view().name(VIEW_SPECIALTY_ADMIN_LIST));
 	}
 
-	@WithMockUser(value = "spring")
+	@WithMockUser(value = "admin", authorities = {
+		"admin"
+	})
 	@Test
 	void testInitCreationForm() throws Exception {
 		mockMvc.perform(MockMvcRequestBuilders.get("/specialty/admin/new"))
@@ -100,7 +72,9 @@ public class SpecialtyAdminControllerTests {
 			.andExpect(MockMvcResultMatchers.view().name(VIEW_SPECIALTY_ADMIN_CREATEORUPDATE));
 	}
 
-	@WithMockUser(value = "spring")
+	@WithMockUser(value = "admin", authorities = {
+		"admin"
+	})
 	@Test
 	void testProcessCreationFormPositive() throws Exception {
 		mockMvc
@@ -110,7 +84,9 @@ public class SpecialtyAdminControllerTests {
 			.andExpect(MockMvcResultMatchers.view().name(VIEW_SPECIALTY_ADMIN_LIST));
 	}
 
-	@WithMockUser(value = "spring")
+	@WithMockUser(value = "admin", authorities = {
+		"admin"
+	})
 	@Test
 	void testProcessCreationFormNegativeNameIsEmpty() throws Exception {
 		mockMvc
@@ -123,7 +99,9 @@ public class SpecialtyAdminControllerTests {
 			.andExpect(MockMvcResultMatchers.view().name(VIEW_SPECIALTY_ADMIN_CREATEORUPDATE));
 	}
 
-	@WithMockUser(value = "spring")
+	@WithMockUser(value = "admin", authorities = {
+		"admin"
+	})
 	@Test
 	void testInitUpdateFormPositive() throws Exception {
 		mockMvc.perform(MockMvcRequestBuilders.get("/specialty/admin/edit/{specialtyId}", TEST_SPECIALTY_ID_1))
@@ -132,7 +110,9 @@ public class SpecialtyAdminControllerTests {
 			.andExpect(MockMvcResultMatchers.view().name(VIEW_SPECIALTY_ADMIN_CREATEORUPDATE));
 	}
 
-	@WithMockUser(value = "spring")
+	@WithMockUser(value = "admin", authorities = {
+		"admin"
+	})
 	@Test
 	void testInitUpdateFormValueNotPresent() throws Exception {
 		mockMvc.perform(MockMvcRequestBuilders.get("/specialty/admin/edit/{specialtyId}", TEST_SPECIALTY_ID_99))
@@ -140,7 +120,9 @@ public class SpecialtyAdminControllerTests {
 			.andExpect(MockMvcResultMatchers.view().name(VIEW_REDIRECT_OUPS));
 	}
 
-	@WithMockUser(value = "spring")
+	@WithMockUser(value = "admin", authorities = {
+		"admin"
+	})
 	@Test
 	void testProcessUpdateFormPositive() throws Exception {
 		mockMvc
@@ -150,7 +132,9 @@ public class SpecialtyAdminControllerTests {
 			.andExpect(MockMvcResultMatchers.view().name(VIEW_SPECIALTY_ADMIN_LIST));
 	}
 
-	@WithMockUser(value = "spring")
+	@WithMockUser(value = "admin", authorities = {
+		"admin"
+	})
 	@Test
 	void testProcessUpdateFormNegativeNameIsEmpty() throws Exception {
 		mockMvc
@@ -163,7 +147,9 @@ public class SpecialtyAdminControllerTests {
 			.andExpect(MockMvcResultMatchers.view().name(VIEW_SPECIALTY_ADMIN_CREATEORUPDATE));
 	}
 
-	@WithMockUser(value = "spring")
+	@WithMockUser(value = "admin", authorities = {
+		"admin"
+	})
 	@Test
 	void testProcessUpdateFormValueSpecialtyNotPresent() throws Exception {
 		mockMvc
@@ -173,7 +159,9 @@ public class SpecialtyAdminControllerTests {
 			.andExpect(MockMvcResultMatchers.view().name(VIEW_REDIRECT_OUPS));
 	}
 
-	@WithMockUser(value = "spring")
+	@WithMockUser(value = "admin", authorities = {
+		"admin"
+	})
 	@Test
 	void testAvailablePositive() throws Exception {
 		mockMvc.perform(MockMvcRequestBuilders.get("/specialty/admin/available/{specialtyId}", TEST_SPECIALTY_ID_1))
@@ -184,7 +172,9 @@ public class SpecialtyAdminControllerTests {
 			.andExpect(MockMvcResultMatchers.view().name(VIEW_SPECIALTY_ADMIN_LIST));
 	}
 
-	@WithMockUser(value = "spring")
+	@WithMockUser(value = "admin", authorities = {
+		"admin"
+	})
 	@Test
 	void testAvailableValueNotPresent() throws Exception {
 		mockMvc.perform(MockMvcRequestBuilders.get("/specialty/admin/available/{specialtyId}", TEST_SPECIALTY_ID_99))
@@ -192,7 +182,9 @@ public class SpecialtyAdminControllerTests {
 			.andExpect(MockMvcResultMatchers.view().name(VIEW_REDIRECT_OUPS));
 	}
 
-	@WithMockUser(value = "spring")
+	@WithMockUser(value = "admin", authorities = {
+		"admin"
+	})
 	@Test
 	void testNotAvailablePositive() throws Exception {
 		mockMvc.perform(MockMvcRequestBuilders.get("/specialty/admin/notAvailable/{specialtyId}", TEST_SPECIALTY_ID_1))
@@ -203,11 +195,14 @@ public class SpecialtyAdminControllerTests {
 			.andExpect(MockMvcResultMatchers.view().name(VIEW_SPECIALTY_ADMIN_LIST));
 	}
 
-	@WithMockUser(value = "spring")
+	@WithMockUser(value = "admin", authorities = {
+		"admin"
+	})
 	@Test
 	void testNotAvailableValueNotPresent() throws Exception {
 		mockMvc.perform(MockMvcRequestBuilders.get("/specialty/admin/notAvailable/{specialtyId}", TEST_SPECIALTY_ID_99))
 			.andExpect(MockMvcResultMatchers.status().is3xxRedirection())
 			.andExpect(MockMvcResultMatchers.view().name(VIEW_REDIRECT_OUPS));
 	}
+
 }
