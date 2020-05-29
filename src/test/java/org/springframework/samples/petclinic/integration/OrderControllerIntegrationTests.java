9de1fe7c6cb170dcfc7c5794d0d1fbe682910c6c
cargamram <cargamram@alum.us.es>
2020-05-29 19:23:18 +0200

    Corregido error en el test de OrderControllerIntegrationTests

--- src/test/java/org/springframework/samples/petclinic/integration/OrderControllerIntegrationTests.java
+++ src/test/java/org/springframework/samples/petclinic/integration/OrderControllerIntegrationTests.java
@@ -150,9 +150,8 @@ public class OrderControllerIntegrationTests {
 
 		Manager manager = managerService.findPersonByUsername(SessionUtils.obtainUserInSession().getUsername());
 		Collection<Order> orders = orderService.findAllOrdersByManagerId(manager.getId());
-		List<Order> order = orders.stream().filter(o -> o.getDate().equals(LocalDate.now())
-			&& o.getIsAccepted().equals(false) && o.getManager().getId().equals(manager.getId()))
-			.collect(Collectors.toList());
+		List<Order> order = orders.stream().filter(o -> o.getDate().equals(LocalDate.now()) && o.getIsAccepted() == null
+			&& o.getManager().getId().equals(manager.getId())).collect(Collectors.toList());
 
 		Collection<ProductOrder> productOrders = productOrderService.findProductOrderByOrder(order.get(0).getId());
 		productOrders.forEach(po -> {
